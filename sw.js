44fcdea2bc5f247b8fd27a6cfa44cca78c44eccb
ScottFlemingCH <30761939+ScottFlemingCH@users.noreply.github.com>
2025-12-16 10:40:44 -0600

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'app-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
